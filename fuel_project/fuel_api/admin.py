9ecc1393b858e0a826fb2d8a6d8bc72099d9dc19
saadnaseem97 <31837782+saadnaseem97@users.noreply.github.com>
2025-02-01 13:35:21 -0500

    Delete fuel_project directory

--- fuel_project/fuel_api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import FuelStation
-
-@admin.register(FuelStation)
-class FuelStationAdmin(admin.ModelAdmin):
-    list_display = ('truckstop_name', 'city', 'state', 'retail_price', 
-                    'latitude', 'longitude', 'geocode_success')
-    search_fields = ('truckstop_name', 'city', 'state')
-    list_filter = ('state', 'geocode_success')
--- fuel_project/fuel_project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from fuel_api import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/plan_route/', views.plan_route, name='plan_route'),
-    path('', views.map_view, name='map'),
-]
